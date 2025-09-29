0855e1b4dc016b06e62df1873105dbd049e711a4
Lê Phi Trường <24521894@gm.uit.edu.vn>
2025-09-29 14:46:47 +0700

    4 ô giao diện

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+
+namespace MyMoney
+{
+    public partial class MainWindow : Window
+    {
+        private decimal ThuNhap = 0;
+        private decimal ChiTieu = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            CapNhatThongTin();
+        }
+
+        private void CapNhatThongTin()
+        {
+            decimal SoDu = ThuNhap - ChiTieu;
+            decimal TiLeTietKiem = ThuNhap > 0 ? (SoDu / ThuNhap) * 100 : 0;
+            
+            txtThuNhap.Text = $"{ThuNhap:N0}₫";
+            txtChiTieu.Text = $"{ChiTieu:N0}₫";
+            txtSoDu.Text = $"{SoDu:N0}₫";
+            txtTiLeTietKiem.Text = $"{TiLeTietKiem:F1}%";
+        }
+    }
+}
